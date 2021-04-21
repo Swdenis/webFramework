c5a196e172a96bf025cf9031be79f2bdfa440e52
Swdenis <swdenis@mail.ru>
2021-04-21 12:16:15 +0200

    Refactor with composition

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { User } from "./models/User";
 
 const testData = {id: 1, name: 'Alex', age: 24}
 const testData2 = {name: 'NewUser', age: 12}
-const updateData = {age: 81}
-const user = new User(testData2)
 
-user.save()
+const user = new User(testData)
+user.events.on('change',()=> {
+    console.log('change')
+})
+
+user.events.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,13 @@
-import axios, { AxiosResponse } from 'axios'
-interface UserProps {
+import { Eventing } from './Eventing'
+export interface UserProps {
     id?: number,
     name?: string,
     age?: number
 }
 
-type Callback = () => void
-
 export class User {
 
-    events: {[key: string]: Callback[]} = {}
+    events: Eventing = new Eventing()
 
     constructor(private data: UserProps) {}
 
@@ -19,33 +17,5 @@ export class User {
 
     set(userProps: UserProps): void {
         Object.assign(this.data, userProps)
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || []
-        handlers.push(callback)
-        this.events[eventName] = handlers
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName]
-        if(!handlers || handlers.length === 0) return
-        handlers.forEach(callback => callback())
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-        .then((response: AxiosResponse): void => {
-            this.set(response.data)
-        })
-    }
-
-    save(): void {
-        const id = this.get('id')
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`, this.data)
-        } else {
-            axios.post('http://localhost:3000/users', this.data)
-        }
     }
 }
