328f31002ef474931cfc9a0b4b8146cb2b0e8c9d
Swdenis <swdenis@mail.ru>
2021-04-21 10:30:09 +0200

    Completed eventing system

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hi there')
+import { User } from "./models/User";
+
+const testData = {name: 'Alex', age: 24}
+const updateData = {age: 81}
+const user = new User(testData)
+
+
+user.on('change', ()=>{
+    console.log('change1')
+})
+user.on('change', ()=>{
+    console.log('change2')
+})
+user.on('save', ()=>{
+    console.log('save1')
+})
+
+user.trigger('change')
+user.trigger('save')
+user.trigger('hey')
---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+interface UserProps {
+    name?: string,
+    age?: number
+}
+
+type Callback = () => void
+
+export class User {
+
+    events: {[key: string]: Callback[]} = {}
+
+    constructor(private data: UserProps) {}
+
+    get(propName: string): (string | number) {
+        return this.data[propName]
+    }
+
+    set(userProps: UserProps): void {
+        Object.assign(this.data, userProps)
+    }
+
+    on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || []
+        handlers.push(callback)
+        this.events[eventName] = handlers
+    }
+
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName]
+        if(!handlers || handlers.length === 0) return
+        handlers.forEach(callback => callback())
+    }
+}
