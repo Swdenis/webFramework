48f61a39dcaafeea2f6a7c458af0ae4834a901ed
Swdenis <swdenis@mail.ru>
2021-04-21 11:23:09 +0200

    Create save method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { User } from "./models/User";
 
 const testData = {id: 1, name: 'Alex', age: 24}
+const testData2 = {name: 'NewUser', age: 12}
 const updateData = {age: 81}
-const user = new User(testData)
+const user = new User(testData2)
 
-user.fetch()
+user.save()
