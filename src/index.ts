37acf4c1c7d86cd6e607eb35077cf8a72bbda683
Swdenis <swdenis@mail.ru>
2021-04-21 11:16:44 +0200

    Added fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 import { User } from "./models/User";
 
-const testData = {name: 'Alex', age: 24}
+const testData = {id: 1, name: 'Alex', age: 24}
 const updateData = {age: 81}
 const user = new User(testData)
 
-
-user.on('change', ()=>{
-    console.log('change1')
-})
-user.on('change', ()=>{
-    console.log('change2')
-})
-user.on('save', ()=>{
-    console.log('save1')
-})
-
-user.trigger('change')
-user.trigger('save')
-user.trigger('hey')
+user.fetch()
