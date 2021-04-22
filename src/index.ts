286aa009f3dbc53660d78f8471017355b6b8fbcc
Swdenis <swdenis@mail.ru>
2021-04-22 11:30:16 +0200

    Refactor user with delegation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { User } from "./models/User";
 
-const testData = {id: 1, name: 'Alex', age: 24}
+const testData = {id: 1, name: 'New name', age: 24}
 
 const user = new User(testData)
+
+user.on('save', ()=>{console.log(user)})
+user.save()
