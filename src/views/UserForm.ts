bf4c7f9890f20520cde4d17bccad6b93c04bf64e
Swdenis <swdenis@mail.ru>
2021-04-23 11:07:51 +0200

    Resolved strict null checks

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -17,6 +17,16 @@ export class UserForm {
     eventsMap(): {[key: string]: ()=> void } {
         return {
             'click:.set-age': this.onSetAgeClick,
+            'click:.set-name': this.onSetNameClick,
+        }
+    }
+
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input')
+
+        if (input) {
+            const name = input.value
+            this.model.set({name})
         }
     }
 
@@ -31,6 +41,7 @@ export class UserForm {
             <div>User Name: ${this.model.get('name')}</div>
             <div>User Age: ${this.model.get('age')}</div>
             <input />
+            <button class="set-name">Change Name</button>
             <button class="set-age">Set Random Age</button>
         </div>
         `
